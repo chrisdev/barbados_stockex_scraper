32732028c1e70e90df39863aa379b9a47859fa28
Christopher Clarke <cclarke@chrisdev.com>
2014-07-08 11:27:12 -0400

    Basic project setup

---
+++ barbados_stockex_scraper/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for barbados_stockex_scraper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'barbados_stockex_scraper'
+
+SPIDER_MODULES = ['barbados_stockex_scraper.spiders']
+NEWSPIDER_MODULE = 'barbados_stockex_scraper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'barbados_stockex_scraper (+http://www.yourdomain.com)'
